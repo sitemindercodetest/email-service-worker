a4a8176ce004d1b36639c01145c3741658226a7a
Ashok Ramdoss <ashok.contact@gmail.com>
2019-10-05 20:43:00 +0100

    Add SQS Listener

---
+++ src/main/java/com/siteminder/worker/listener/EmailSqsListener.java
@@ -0,0 +1,26 @@
+package com.siteminder.worker.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.siteminder.worker.model.EmailRequest;
+import com.siteminder.worker.service.EmailProviderManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import static org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy.ON_SUCCESS;
+
+@Component
+public class EmailSqsListener {
+    private final Logger logger = LoggerFactory.getLogger(EmailSqsListener.class);
+
+    @Autowired
+    private EmailProviderManager emailProviderManager;
+
+    @SqsListener(value = "${sqs.queueName}", deletionPolicy = ON_SUCCESS)
+    public void getMessage(String message) throws IOException {
+        EmailRequest emailRequest = new ObjectMapper().readValue(message, EmailRequest.class);
+        emailProviderManager.sendEmail(emailRequest);
+    }
+}
